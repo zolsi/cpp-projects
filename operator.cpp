56ceb82d3aef6c0eb3e2ad854a72a5d932c97155
zolsi <khalidmk277@gmail.com>
2024-10-03 14:39:39 +0100

    Update operator.cpp

--- operator.cpp
+++ operator.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    /*int x=4;
+    x <<=3;//bits will be shifted of the number to the left by 4 positions
+    int y=3;
+    y>>=1;
+    cout<<y<<"\n" ;// bits will be shifted to the right
+    */
+     int z=5;
+     z &= 3;
+    cout << z;
+return 0;
+
+
+
+
+    
+}
 
